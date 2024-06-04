4064a4ad25514b9239b6c13360cd63bbce54f7dc
adilraees786 <adilraees911@gmail.com>
2024-06-04 11:52:40 +0500

    To Do list

---
+++ app.js
@@ -0,0 +1,37 @@
+
+var ul = document.getElementById('show')
+var input = document.getElementById('text')
+var button = document.getElementById('btn')
+
+//Creat li
+
+button.addEventListener('click', function(){
+   var li = document.createElement('li')
+
+   var userText = document.createTextNode(input.value)
+   li.appendChild(userText)
+//creat button
+   let addbutton=document.createElement('button')
+   var addtext=document.createTextNode('X')
+   addbutton.appendChild(addtext)
+   li.appendChild(addbutton)
+//end button
+   addbutton.setAttribute('id', 'abc')
+//creat button
+var edit=document.createElement('button')
+var edittext=document.createTextNode('Edit')
+edit.appendChild(edittext)
+li.appendChild(edit)
+//end button
+
+//remove and  show in console.log
+addbutton.addEventListener('click' ,function(e){
+console.log(e.target.id=li);
+li.remove()
+//end
+})
+
+ul.appendChild(li)
+input.value =''
+})
+
